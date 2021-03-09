084411536db1b053bd70e47975d42de0ab16d2cf
dzmitryNz <d.nayezzhy@gmail.com>
2021-03-09 18:55:12 +0300

    feat: footer links add

--- travel-app/src/components/Footer/Footer.tsx
+++ travel-app/src/components/Footer/Footer.tsx
@@ -9,10 +9,18 @@ const Footer = () => {
     return (
         <Container className="footer-block">
        <footer className="d-flex justify-content-between flex-wrap header">
-           <div className="logo-block d-flex">
+        <div className="logo-block d-flex">
            <ImageAlt className="logo text-warning"/>
            <h1 className="head text-primary">Travel App</h1>
-           </div>
+        </div>
+           <a href="https://github.com/LexSava" target="_blank" rel="noopener noreferrer">LexSava</a>
+           <a href="https://github.com/dzmitrynz" target="_blank" rel="noopener noreferrer">dzmitryNz</a>
+           <a href="https://github.com/Anastasia" target="_blank" rel="noopener noreferrer">Anastasia</a>
+           <a href="https://github.com/inex" target="_blank" rel="noopener noreferrer">inex</a>
+           <a className='footer__school' href="https://rs.school/react/">
+           <img src="https://rollingscopes.com/images/logo_rs2.svg" alt="rsschool" />
+           </a>
+           <p>2021</p>
            <Localization/>
        </footer>
        </Container>
