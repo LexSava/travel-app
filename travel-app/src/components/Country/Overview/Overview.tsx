fd4d0bf0a4d230842f5426c7802e16bdf799440b
LexSava <lexsava@gmail.com>
2021-03-17 17:16:46 +0300

    fix: fix code

--- travel-app/src/components/Country/Overview/Overview.tsx
+++ travel-app/src/components/Country/Overview/Overview.tsx
@@ -1,28 +1,27 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Jumbotron, Container, Image } from "react-bootstrap";
-import "./Overview.scss";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Jumbotron, Container, Image } from 'react-bootstrap';
+import './Overview.scss';
 
-import CountryType from "./../../../types/CountryModel";
-import DateWidget from "../../DateWidget/DateWidget";
-import ExchangeRates from "../../ExchangeRates/ExchangeRates";
-import Weather from "../../Weather/Weather";
-import Video from "../../Video/Video";
+import { ICountries } from './../../../utils/interfaces';
+import CountryType from './../../../types/CountryModel';
+import DateWidget from '../../DateWidget/DateWidget';
+import Weather from '../../Weather/Weather';
+import Video from '../../Video/Video';
 
 type OverviewProps = {
-  countryInfo: any;
+  countryInfo: ICountries;
   conveyLanguage: string;
 };
 
 const Overview = ({ countryInfo, conveyLanguage }: OverviewProps) => {
   return (
-    <div className="country-overview">
-      <Jumbotron fluid style={{ backgroundImage: `url(${countryInfo.img})` }}>
-        <Container className="position-relative">
-          <Container className="d-flex widget-block mb-2">
-            <ExchangeRates
-              conveyLanguage={conveyLanguage}
-              countryInfo={countryInfo}
-            />
+    <div className='country-overview'>
+      <Jumbotron
+        fluid
+        style={{ backgroundImage: `url(${countryInfo.photoSrc1})` }}
+      >
+        <Container className='position-relative'>
+          <Container className='d-flex widget-block mb-2'>
             <DateWidget
               conveyLanguage={conveyLanguage}
               countryInfo={countryInfo}
@@ -32,23 +31,23 @@ const Overview = ({ countryInfo, conveyLanguage }: OverviewProps) => {
               conveyLanguage={conveyLanguage}
             />
           </Container>
-          <div className="country-overview__main">
+          <div className='country-overview__main'>
             <Image
-              className="country-overview__main__image"
+              className='country-overview__main__image'
               src={countryInfo.photoSrc1}
               rounded
             />
-            <div className="country-overview__main__content">
-              <div className="country-overview__main__content__capital">
-                <h1 className="text-primary overview-title">
+            <div className='country-overview__main__content'>
+              <div className='country-overview__main__content__capital'>
+                <h1 className='text-primary overview-title'>
                   {countryInfo.nameEn}
                 </h1>
-                <h2 className="text-capital">{countryInfo.capitalEn}</h2>
+                <h2 className='text-capital'>{countryInfo.capitalEn}</h2>
               </div>
             </div>
           </div>
 
-          <p className="country-overview__description">
+          <p className='country-overview__description'>
             {countryInfo.articleEn}
           </p>
           <Video videoSrc={countryInfo.videoEn} />
